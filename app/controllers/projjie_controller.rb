10be6dc7beb35d191e8ab6fe1a081bcf99512411
Adam McLean <adam.mclean1@gmail.com>
2015-02-13 09:28:41 -0800

    first commit

---
+++ app/controllers/projjie_controller.rb
@@ -0,0 +1,27 @@
+class ProjjieController < ApplicationController
+  def home
+  	@user = User.all
+  end
+
+  def about
+  end
+
+  def new
+ 	end
+
+ 	def edit
+ 	end
+
+ 	def create
+ 	end
+
+ 	def show
+ 	end
+
+ 	def update
+ 	end
+
+ 	def destroy
+ 	end
+
+end
---
+++ app/helpers/projjie_helper.rb
@@ -0,0 +1,2 @@
+module ProjjieHelper
+end
